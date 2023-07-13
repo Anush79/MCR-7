b4756ed934c4806f5e4d3049e24103a031cb135c
Anush79 <barbieworld1056.a@gmail.com>
2023-07-13 18:23:21 +0530

    Add: footer

---
+++ src/component/Footer.jsx
@@ -0,0 +1,7 @@
+import { NavLink } from "react-router-dom";
+
+export default function Footer(){
+  return <footer>
+    <small>Made with React by <NavLink to="https://github.com/Anush79/MCR-7">Anushka</NavLink></small>
+  </footer>
+}
